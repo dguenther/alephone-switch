2c82df8fda508ce75a17c757e5ef48c760140c95
Derek Guenther <dguenther9@gmail.com>
2026-03-08 21:46:13 -0400

    move containerfile

--- scripts/switch-container-build.sh
+++ scripts/switch-container-build.sh
@@ -4,7 +4,7 @@ set -euo pipefail
 ROOT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")/.." && pwd)"
 IMAGE_TAG="${IMAGE_TAG:-alephone-switch}"
 
-podman build -t "${IMAGE_TAG}" -f "${ROOT_DIR}/containers/switch/Containerfile" "${ROOT_DIR}"
+podman build -t "${IMAGE_TAG}" -f "${ROOT_DIR}/switch/Containerfile" "${ROOT_DIR}"
 
 podman run --rm \
   -v "${ROOT_DIR}":/work:Z \
